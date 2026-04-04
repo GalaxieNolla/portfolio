eb3819653dc9a63035979e79fda9b6dc815af005
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-04-04 16:11:05 -0700

    Update and rename allEducations.js to Education.js

---
+++ .content-collections/generated/Education.js
@@ -0,0 +1,16 @@
+
+export default [
+  {
+    "school": "University of California, Berkeley",
+    "summary": "B.A. Data Science, B.A. Political Science",
+    "endDate": "2026-08",
+    "tags": [
+      "Cybersecurity",
+      "AI Ethics",
+      "National Security",
+      "Algorithmic Bias",
+      "Tech Governance"
+    ],
+    "content": "Focusing on the intersection of data science, public policy, and national security. Active in Berke1337 Cybersecurity Club, the Alexander Hamilton Society (Executive Officer), and the Berkeley Risk and Security Lab.",
+  }
+]
