9782b18104fe1cb767af33fd4ba73f1947b5289b
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-04-04 20:15:31 -0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -57,8 +57,9 @@ function Home() {
           <p className="text-slate-400 text-lg md:text-xl max-w-2xl leading-relaxed mb-10">
             Software Engineer and Researcher at the Berkeley Risk and Security Lab, focused on building resilient systems at the intersection of tech and policy. 
             I’m driven by a deep curiosity for how digital architectures shape our world—and a commitment towards model evaluation & algorithmic accountability. 
-            I thrive on translating complex, high-stakes problems into clean, concise, and effective code. <br />
-
+            <br /><br />
+            I thrive on translating complex, high-stakes problems into clean, concise, and effective code.
+            <br /><br />
             In my free time, I enjoy feeding my creative spirit.
           </p>
           
