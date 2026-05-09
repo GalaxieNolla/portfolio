42f0b640e355e8a9a6d757c7c9d6a3a169a2ab67
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-05-09 11:37:10 -0700

    add flow-state

--- .content-collections/generated/allProjects.js
+++ .content-collections/generated/allProjects.js
@@ -1,1 +1,18 @@
-export default[]
+export default [
+  {
+    "title": "Flow State",
+    "description": "An Arcane-inspired productivity app that gamifies studying.",
+    "tags": [
+      "Productivity",
+      "Gamification",
+      "Pomodoro",
+      "Focus",
+      "Personal Project",
+      "python",
+      "League of Legends"
+    ],
+    "github": "https://github.com/GalaxieNolla/flow-state",
+    "liveUrl": "https://youtu.be/JJk673GNfQ4",
+    "content": "An Arcane-inspired productivity app that gamifies studying with a Pomodoro timer, task list, and real-time distraction monitoring. App includes a Pomodoro timer, task list, and real-time distraction monitoring with nudges to keep you in the flow stateIncludes a full demo and files breakdown."
+  }
+]
